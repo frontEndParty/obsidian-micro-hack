4610af94ef1dd7f7b99acb9bec45aab77ddafb3e
Nick Amante <namante@gmail.com>
2025-01-14 19:29:02 -0600

    Fuck it, ship it.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,25 @@
-/**
- * Make your Plugin here!
- * GLHF!
- */
+import { Menu, Notice, Plugin } from 'obsidian';
+
+export default class CopyImageShortcut extends Plugin {
+  private clickImage = (event: MouseEvent) => {
+    console.log('click');
+  }
+
+  async onload() {
+   
+    this.addRibbonIcon('copy', 'Open menu', (event) => {
+      const menu = new Menu();
+
+      menu.addItem((item) =>
+        item
+          .setTitle('Copy Image')
+          .setIcon('documents')
+          .onClick(() => {
+            new Notice('Copied');
+          })
+      );
+
+      menu.showAtMouseEvent(event);
+    });
+  }
+}
