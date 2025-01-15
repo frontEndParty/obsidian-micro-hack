16f2352da6449c75ea6de92b41ef1c7ee4891725
ninatran <ninatran.az@gmail.com>
2025-01-14 19:39:15 -0600

    (add) feature

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,45 @@
  * Make your Plugin here!
  * GLHF!
  */
+import { Notice, Plugin } from "obsidian";
+
+export default class MyPlugin extends Plugin {
+  async onload() {
+    this.addRibbonIcon("dice", "Greet", () => {
+      new Notice("Hello, world! 2");
+    });
+
+    setTimeout(() => {
+      const images = document.querySelectorAll(".internal-embed img");
+      console.log(images);
+
+      images[0].addEventListener("contextmenu", async (e) => {
+        const responsePromise = fetch(e.target.src);
+        try {
+          if ("write" in navigator.clipboard) {
+            await navigator.clipboard.write([
+              new ClipboardItem({
+                "image/png": new Promise(async (resolve) => {
+                  const blob = await responsePromise.then((response) =>
+                    response.blob()
+                  );
+                  resolve(blob);
+                }),
+              }),
+            ]);
+            // Image copied as image.
+          } else {
+            const text = await responsePromise.then((response) =>
+              response.text()
+            );
+            await navigator.clipboard.writeText(text);
+            // Image copied as source code.
+          }
+          console.log("image copied");
+        } catch (err) {
+          console.error(err.name, err.message);
+        }
+      });
+    }, 1000);
+  }
+}
